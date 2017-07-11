b5fbf478a8955a63dc501f85c1b4a868f6f4a7ba
Ilya Verbitskiy <ilya.verbitskiy@gmail.com>
2017-07-11 12:09:31 +0200

    init connected components

---
+++ scripts/runtime-adjustments-cc.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+HADOOP_HOME=${HADOOP_HOME:-/home/ilya/hadoop-2.7.3}
+SPARK_HOME=${SPARK_HOME:-/home/ilya/spark-2.1.0-bin-hadoop2.7}
+
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+
+for (( i = 0; i < 3; i++ )); do
+
+    echo "Executing run $i..."
+
+    $HADOOP_HOME/bin/hdfs dfs -rm -R /cc-out
+    RUN_NAME=run$(printf %02d $i)
+
+    $SPARK_HOME/bin/spark-submit \
+    --master yarn \
+    --deploy-mode cluster \
+    --executor-cores 8 \
+    --properties-file conf/spark-defaults.conf \
+    --class de.tuberlin.cit.jobs.ConnectedComponents \
+    ../target/runtime-adjustments-experiments-1.0-SNAPSHOT-jar-with-dependencies.jar \
+    --min-containers 4 \
+    --max-containers 24 \
+    --max-runtime 360000 \
+    --iterations 10 \
+    --db "/home/ilya/runtime-adjustments-experiments/target/bell" \
+    hdfs://wally046:45010//uk-2007-05.txt \
+    hdfs://wally046:45010//cc-out \
+    > logs/${RUN_NAME}.out 2> logs/${RUN_NAME}.log
+
+done
---
+++ src/main/scala/de/tuberlin/cit/jobs/ConnectedComponents.scala
@@ -0,0 +1,77 @@
+package de.tuberlin.cit.jobs
+
+import de.tuberlin.cit.adjustments.StageScaleOutPredictor
+import org.apache.spark.graphx.{Graph, VertexId}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import org.rogach.scallop.exceptions.ScallopException
+import org.rogach.scallop.{ScallopConf, ScallopOption}
+
+object ConnectedComponents {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new ConnectedComponentsArgs(args)
+    val appSignature = "CC"
+
+    val sparkConf = new SparkConf()
+      .setAppName(appSignature)
+    val sparkContext = new SparkContext(sparkConf)
+    val listener = new StageScaleOutPredictor(
+      sparkContext,
+      appSignature,
+      conf.dbPath(),
+      conf.minContainers(),
+      conf.maxContainers(),
+      conf.maxRuntime(),
+      conf.adaptive())
+    sparkContext.addSparkListener(listener)
+
+    sparkContext.textFile(conf.input())
+    val data = sparkContext.textFile(conf.input())
+    val edges: RDD[(VertexId, VertexId)] = data.map(s => {
+      val arr = s.split("\\s").map(_.toLong)
+      (arr(0), arr(1))
+    })
+    val graph: Graph[Int, Int] = Graph.fromEdgeTuples(edges, 1)
+    val result: Graph[VertexId, Int] = graph.connectedComponents(conf.iterations())
+
+    result.vertices.saveAsTextFile(conf.output())
+  }
+}
+
+class ConnectedComponentsArgs(a: Seq[String]) extends ScallopConf(a) {
+  //  val config = opt[String](required = true,
+  //    descr = "Path to the .conf file")
+  //
+  val dbPath: ScallopOption[String] = opt[String](name = "db", noshort = true, descr = "Path to the H2 database",
+    default = Some("./target/bell"))
+  val input: ScallopOption[String] = trailArg[String](required = true, name = "<input>",
+    descr = "Input file").map(_.toLowerCase)
+  val output: ScallopOption[String] = trailArg[String](required = true, name = "<output>",
+    descr = "Output file").map(_.toLowerCase)
+  val maxRuntime: ScallopOption[Int] = opt[Int](required = true, short = 'r',
+    descr = "Maximum runtime in milliseconds")
+  val minContainers: ScallopOption[Int] = opt[Int](short = 'n', default = Option(1),
+    descr = "Minimum number of containers to assign")
+  val maxContainers: ScallopOption[Int] = opt[Int](required = true, short = 'N',
+    descr = "Maximum number of containers to assign")
+
+  val iterations: ScallopOption[Int] = opt[Int](noshort = true, default = Option(100),
+    descr = "Amount of KMeans iterations")
+  val cache: ScallopOption[Boolean] = opt[Boolean](noshort = true, default = Option(false),
+    descr = "Caches the input data")
+  val adaptive: ScallopOption[Boolean] = opt[Boolean](default = Option(false), noshort = true,
+    descr = "Enables adjustments by scaling between jobs")
+
+  override def onError(e: Throwable): Unit = e match {
+    case ScallopException(message) =>
+      println(message)
+      println()
+      printHelp()
+      System.exit(1)
+    case other => super.onError(e)
+  }
+
+  verify()
+}
+
