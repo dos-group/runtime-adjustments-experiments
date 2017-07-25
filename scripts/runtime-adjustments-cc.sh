63b8f32ed20f68034be4eac43cc6303b22e1ca54
Ilya Verbitskiy <ilya.verbitskiy@gmail.com>
2017-07-25 14:44:58 +0200

    use tcp version of H2 db for CC

--- scripts/runtime-adjustments-cc.sh
+++ scripts/runtime-adjustments-cc.sh
@@ -5,11 +5,10 @@ SPARK_HOME=${SPARK_HOME:-/home/ilya/spark-2.1.0-bin-hadoop2.7}
 
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 
-for (( i = 0; i < 3; i++ )); do
+for (( i = 0; i < 2; i++ )); do
 
     echo "Executing run $i..."
 
-    $HADOOP_HOME/bin/hdfs dfs -rm -R /cc-out
     RUN_NAME=run$(printf %02d $i)
 
     $SPARK_HOME/bin/spark-submit \
@@ -19,12 +18,12 @@ for (( i = 0; i < 3; i++ )); do
     --properties-file conf/spark-defaults.conf \
     --class de.tuberlin.cit.jobs.ConnectedComponents \
     ../target/runtime-adjustments-experiments-1.0-SNAPSHOT-jar-with-dependencies.jar \
-    --min-containers 4 \
-    --max-containers 24 \
-    --max-runtime 3600000 \
+    --min-containers 10 \
+    --max-containers 50 \
+    --max-runtime 360000 \
     --iterations 10 \
-    --db "/home/ilya/runtime-adjustments-experiments/target/bell" \
-    hdfs://wally020:45010//twitter_rv.net \
+    --db "tcp://130.149.249.30:9092/~/bell" \
+    hdfs://wally020:45010//google_gengraph_25.txt \
     > logs/${RUN_NAME}.out 2> logs/${RUN_NAME}.log
 
 done
