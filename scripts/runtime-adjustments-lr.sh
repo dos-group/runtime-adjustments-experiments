daa7339b510ee72b118e1353ea905793d8de6928
Ilya Verbitskiy <ilya.verbitskiy@gmail.com>
2017-08-02 16:47:00 +0200

    init logistic regression

---
+++ scripts/runtime-adjustments-lr.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+HADOOP_HOME=${HADOOP_HOME:-/home/ilya/hadoop-2.7.3}
+SPARK_HOME=${SPARK_HOME:-/home/ilya/spark-2.1.0-bin-hadoop2.7}
+
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+
+#$HADOOP_HOME/bin/hadoop fs -copyFromLocal /data/ilya/datasets/sgd.txt /
+
+for (( i = 0; i < 2; i++ )); do
+
+    echo "Executing run $i..."
+
+    RUN_NAME=run$(printf %02d $i)
+
+    $SPARK_HOME/bin/spark-submit \
+    --master yarn \
+    --deploy-mode cluster \
+    --executor-cores 8 \
+    --properties-file conf/spark-defaults.conf \
+    --class de.tuberlin.cit.jobs.LogisticRegression \
+    ../target/runtime-adjustments-experiments-1.0-SNAPSHOT-jar-with-dependencies.jar \
+    --min-containers 4 \
+    --max-containers 50 \
+    --max-runtime 300000 \
+    --iterations 20 \
+    --adaptive \
+    --db "tcp://130.149.249.30:9092/~/bell" \
+    hdfs://wally020:45010//multiclass.txt \
+    > logs/${RUN_NAME}.out 2> logs/${RUN_NAME}.log
+
+done
---
+++ src/main/scala/de/tuberlin/cit/jobs/LogisticRegression.scala
@@ -0,0 +1,104 @@
+package de.tuberlin.cit.jobs
+
+import de.tuberlin.cit.adjustments.StageScaleOutPredictor
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.util.MLUtils
+import org.rogach.scallop.exceptions.ScallopException
+import org.rogach.scallop.{ScallopConf, ScallopOption}
+
+object LogisticRegression {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new LRArgs(args)
+
+    val appSignature = "LR"
+
+    val sparkConf = new SparkConf()
+      .setAppName(appSignature)
+    //      .set("spark.shuffle.service.enabled", "true")
+    //      .set("spark.dynamicAllocation.enabled", "true")
+
+    val sparkContext = new SparkContext(sparkConf)
+    val listener = new StageScaleOutPredictor(
+      sparkContext,
+      appSignature,
+      conf.dbPath(),
+      conf.minContainers(),
+      conf.maxContainers(),
+      conf.maxRuntime().toInt,
+      conf.adaptive())
+    sparkContext.addSparkListener(listener)
+
+
+    var data = MLUtils.loadLabeledPoints(sparkContext, conf.input())
+    if (conf.cache()) {
+      data = data.cache()
+    }
+    // Split data into training (60%) and test (40%).
+    val Array(training, test) = data.randomSplit(Array(0.8, 0.2))
+
+    // Run training algorithm to build the model
+    val lr = new LogisticRegressionWithLBFGS()
+      .setNumClasses(3)
+    lr.optimizer
+      .setNumIterations(conf.iterations())
+      .setConvergenceTol(Double.MinValue)
+    val model = lr.run(training)
+
+    // Compute raw scores on the test set.
+    val predictionAndLabels = test.map { case LabeledPoint(label, features) =>
+      val prediction = model.predict(features)
+      (prediction, label)
+    }
+
+    // Get evaluation metrics.
+    val metrics = new MulticlassMetrics(predictionAndLabels)
+    val accuracy = metrics.accuracy
+    println(s"Accuracy = $accuracy")
+  }
+}
+
+class LRArgs(a: Seq[String]) extends ScallopConf(a) {
+  //  val config = opt[String](required = true,
+  //    descr = "Path to the .conf file")
+  //
+  val dbPath: ScallopOption[String] = opt[String](name = "db", noshort = true, descr = "Path to the H2 database", default = Some("./target/bell"))
+  val input: ScallopOption[String] = trailArg[String](required = true, name = "<input>",
+    descr = "Input file").map(_.toLowerCase)
+  val maxRuntime: ScallopOption[Double] = opt[Double](required = true, short = 'r',
+    descr = "Maximum runtime in seconds")
+  val minContainers: ScallopOption[Int] = opt[Int](short = 'n', default = Option(1),
+    descr = "Minimum number of containers to assign")
+  val maxContainers: ScallopOption[Int] = opt[Int](required = true, short = 'N',
+    descr = "Maximum number of containers to assign")
+
+  //  val memory = opt[Int](
+  //    descr = "Memory per container, in MB")
+  //  val masterMemory = opt[Int](short = 'M',
+  //    descr = "Master memory, in MB (Flink JobManager or Spark driver)")
+  //  val slots = opt[Int](
+  //    descr = "Number of slots per TaskManager")
+  val iterations: ScallopOption[Int] = opt[Int](noshort = true, default = Option(100),
+    descr = "Amount of SGD iterations")
+  val cache: ScallopOption[Boolean] = opt[Boolean](noshort = true, default = Option(false),
+    descr = "Caches the input data")
+  val adaptive: ScallopOption[Boolean] = opt[Boolean](default = Option(false), noshort = true,
+    descr = "Enables runtime adjustments by scaling between jobs")
+
+  override def onError(e: Throwable): Unit = e match {
+    case ScallopException(message) =>
+      println(message)
+      //      println(s"Usage: allocation-assistant -c <config> -r <max runtime> -m <memory> -s <slots> " +
+      //        s"-i <fallback containers> -N <max containers> " +
+      //        s"[more args ...] <Jar> [Jar args ...]")
+      println()
+      printHelp()
+      System.exit(1)
+    case other => super.onError(e)
+  }
+
+  verify()
+}
